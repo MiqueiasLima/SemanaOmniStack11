6b59bc1d934feb5f52f3473393fa012006653865
Miqueias Lima <miqueiaslima@outlook.com>
2020-03-27 00:47:57 -0300

    Criando Back-End da Aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+
+const OngController = require('./controllers/OngController');
+const incidentController = require('./controllers/IncidentsController');
+
+const routes = express.Router();
+
+routes.get('/ongs',OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents',incidentController.index);
+routes.post('/incidents',incidentController.create);
+routes.delete('/incidents/:id',incidentController.delete);
+
+module.exports = routes;
